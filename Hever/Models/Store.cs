dc07903d6b43426ffb12b186d5581428daece473
amit.av206 <amit.av206@BMW09>
2019-05-21 17:48:51 +0300

    Add strore model

---
+++ Hever/Models/Store.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hever.Models
+{
+    public class Store
+    {
+        public int StoreId { get; set; }
+
+        public  String StoreName { get; set; }
+
+        public int Address{ get; set; }
+
+        public bool IsAccessible { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
---
+++ Hever/Models/StoreTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace Hever.Models
+{
+    public enum StoreTypes
+    {
+        clothes,
+        shoes,
+        glasses,
+        books,
+        home,
+    }
+}

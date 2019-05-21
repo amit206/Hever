9cde255dfd494ad9fbe26e6f1d7098cc86370c54
Einor <Einor@DESKTOP-MMUJ86C>
2019-05-21 17:28:15 +0300

    add user model

--- Hever/Models/Users.cs
+++ Hever/Models/Users.cs
@@ -7,6 +7,12 @@ namespace Hever.Models
 {
     public class Users
     {
-
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public int Password { get; set; }
+        public string Email { get; set; }
+        public int CardNumber { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public int SVC { get; set; }
     }
 }

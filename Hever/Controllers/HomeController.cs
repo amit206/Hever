96fc3bb4831c75abe020ea1d5441bb0c0704eaea
Amit Avraham <208787135@STUD.colman.ac.il>
2019-08-04 17:54:20 +0300

    add twitter and fix stat

--- Hever/Controllers/HomeController.cs
+++ Hever/Controllers/HomeController.cs
@@ -60,13 +60,5 @@ namespace Hever.Controllers
 
             return View();
         }
-
-        // GET: Home/About
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
--- Hever/Controllers/StatsController.cs
+++ Hever/Controllers/StatsController.cs
@@ -15,17 +15,38 @@ namespace Hever.Controllers
         // GET: Stats
         public ActionResult Index()
         {
-            var StoresByCity = db.Stores.GroupBy(s => s.Area)
-                .Select(i => new { city = i.Key.ToString(), count = i.Count() }).ToList();
-            var GroupByCityJson = JsonConvert.SerializeObject(StoresByCity);
-            ViewBag.GroupByCityJson = GroupByCityJson;
+            var StoresAndResByArea = 
+                db.Stores.GroupBy(s => s.Area)
+                .Select(i => new { city = i.Key.ToString(), count = i.Count() })
 
-            var TypeOfLikedRestaurants = db.Restaurants.GroupBy(r=> r.RestaurantType)
+                .Union(db.Restaurants.GroupBy(s => s.Area)
+                .Select(i => new { city = i.Key.ToString() , count = i.Count() }))
+
+                .GroupBy(s => s.city)
+                .Select(i => new { city = i.Key, count = i.Sum(s => s.count) })
+                .ToList();
+            var GroupByStoresAndResJson = JsonConvert.SerializeObject(StoresAndResByArea);
+            ViewBag.GroupByAreaJson = GroupByStoresAndResJson;
+
+            var TypeOfLikedRestaurants = db.Restaurants.Where(u => u.LikedUsers.Count() != 0)
+                .GroupBy(r=> r.RestaurantType)
+                
                 .Select(i => new { type = i.Key.ToString(), count = i.Count() }).ToList();
             var TypeOfLikedRestaurantsJSON = JsonConvert.SerializeObject(TypeOfLikedRestaurants);
             ViewBag.TypeOfLikedRestaurantsJSON = TypeOfLikedRestaurantsJSON;
 
             return View();
+
+           // IEnumerable<int> likedStore = db.Users.Where(u => u.Id == id).Select(p => p.LikedStores.Select(res => res.Id)).SingleOrDefault().ToList();
+
+            // Get all users that likes at least one store in common
+            //IEnumerable<int> usersStore = db.Users.Where(u => u.LikedStores.Select(str => str.Id).Any(sto => likedStore.Contains(sto))
+           //                          && u.Id != id).Select(uid => uid.Id).ToList();
+
+            // Get all the stores that they like but current user didn't
+           // var recommStores = db.Stores.Where(str => str.LikedUsers.Any(l => usersStore.Contains(l.Id))
+           //                 && !str.LikedUsers.Select(stid => stid.Id).Contains((int)id)).Take(numOfRecords).ToList();
+
         }
     }
 }

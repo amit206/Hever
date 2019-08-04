0398cda2cd9ea0a86bfe21b2655ea4cfc83bb5ef
Amit Avraham <208787135@STUD.colman.ac.il>
2019-08-04 12:46:09 +0300

    merge

---
+++ Hever/Scripts/weather.js
@@ -0,0 +1,11 @@
+﻿var weatherCallback = function (data) {
+    console.log(data);
+    var results = data.query.results
+    var firstResult = results.channel.item.condition
+    console.log(firstResult);
+
+    var location = 'Unknown' // not returned in response
+    var temp = firstResult.temp
+    var text = firstResult.text
+
+}
